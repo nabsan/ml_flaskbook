688e868dfdbfed41f5f14871846164e0b93f11d3
nabsan <nabsan@gmail.com>
2025-03-02 22:28:22 +0900

    py fileから直接起動できるように開発デバッグができる形にしておく。  これなくてもflast runで実用はできていた。

--- flaskbook_api/run.py
+++ flaskbook_api/run.py
@@ -11,3 +11,9 @@ app = Flask(__name__)
 app.config.from_object(config[config_name])
 # blueprintをアプリケーションに登録
 app.register_blueprint(api)
+
+
+#from apps.app import create_app
+#app = create_app("local")
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.getenv("PORT", 5002)), debug=True)
